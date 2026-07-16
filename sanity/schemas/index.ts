8925e1d7133c94a7351f36f9f2285a03b2b80318
ishimwee514-hue <ishimwee514@gmail.com>
2026-07-16 12:29:54 -0700

    Update schema index to include all new content types

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,4 +1,17 @@
 import category from './category'
 import story from './story'
+import legend from './legend'
+import quote from './quote'
+import socialPost from './socialPost'
+import message from './message'
+import libraryItem from './libraryItem'
 
-export const schemaTypes = [category, story]
+export const schemaTypes = [
+  category,
+  story,
+  legend,
+  quote,
+  socialPost,
+  message,
+  libraryItem,
+]
